9f1cea846e180f5fe1a62c7e0505b1f49b83434c
brentanNelson <brentan.nelson@hotmail.com>
2017-03-30 15:21:14 +1100

    fix simons rubbish

--- listener.js
+++ listener.js
@@ -92,7 +92,6 @@ function _initiateLobby() {
 
     $('#preGame').fadeOut(500);
     $('#countdown').delay(500).fadeIn(500, playSound('gamestart'));
-	$('#countdown').delay(57000).fadeIn(500, playSound('gamestart'));
 };
 
 function _gameStarted(game) {
